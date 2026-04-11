c95b8cd8877063f129866c17d2fe0de5c39c4b53
mohammedsbd <mohammed1112ok@gmail.com>
2026-04-11 12:16:56 -0700

    Update new_controller.rb

--- app/controllers/new_controller.rb
+++ app/controllers/new_controller.rb
@@ -5238,7 +5238,48 @@ class Challenge < ApplicationRecord
 class Challenge < ApplicationRecord
 class Challenge < ApplicationRecord
   has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy validates :name, presence: true
+  validates :name, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :description, presence: true
+  validates :description, presence: true
   has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+class Challenge < ApplicationRecord
+class Challenge < ApplicationRecord
+class Challenge < ApplicationRecord
+  has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+class Challenge < ApplicationRecord
+class Challenge < ApplicationRecord
+class Challenge < ApplicationRecord
+  has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+class Challenge < ApplicationRecord
+class Challenge < ApplicationRecord
+class Challenge < ApplicationRecord
+  has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+class Challenge < ApplicationRecord
+class Challenge < ApplicationRecord
+class Challenge < ApplicationRecord
+  has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+class Challenge < ApplicationRecord
+class Challenge < ApplicationRecord
+class Challenge < ApplicationRecord
+  has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+  has_many :challenge_participants, dependent: :destroy
+  has_many :participants, through: :challenge_participants, source: :user
+  has_many :participants, through: :challenge_participants, source: :user
+  has_many :participants, through: :challenge_participants, source: :user
   has_many :challenge_participants, dependent: :destroy
 class Challenge < ApplicationRecord
 class Challenge < ApplicationRecord
